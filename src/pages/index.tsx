c5cb27afcac57fef49e8fc0eee6d1f2ab338b27f
viachess <mr.viacheslav@yandex.ru>
2022-01-01 07:22:10 +0400

    mobile view minor fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
+import styles from "./Home.module.css";
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
